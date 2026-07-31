c9ad8cc85e71e1b3fb7373272fb9922998b0f285
RAGEAQ <MIR264252@GMAIL.COM>
2026-07-31 19:27:23 +0530

    Update script.js

--- css/js/script.js
+++ css/js/script.js
@@ -1,1 +1,29 @@
+function searchProducts(){
 
+let input = document.getElementById("searchBox");
+
+let filter = input.value.toLowerCase();
+
+let products = document.getElementsByClassName("product-card");
+
+
+for(let i=0;i<products.length;i++){
+
+let text = products[i].innerText.toLowerCase();
+
+
+if(text.includes(filter)){
+
+products[i].style.display="block";
+
+}
+
+else{
+
+products[i].style.display="none";
+
+}
+
+}
+
+}
