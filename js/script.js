0f383e6c8c059eab27ec00b5659f12080e2b9f29
RAGEAQ <MIR264252@GMAIL.COM>
2026-08-01 09:29:01 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -102,21 +102,7 @@ card.style.display="none";
    Contact Form
 ========================================== */
 
-const form=document.querySelector("form");
 
-if(form){
-
-form.addEventListener("submit",function(e){
-
-e.preventDefault();
-
-alert("Thank you! Your message has been received.");
-
-form.reset();
-
-});
-
-}
 
 /* ==========================================
    Active Navigation
