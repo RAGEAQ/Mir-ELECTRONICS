d545a3ff4d5cb2e21e2ca56b721326326ac0f8a2
RAGEAQ <MIR264252@GMAIL.COM>
2026-08-02 12:23:48 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -189,3 +189,15 @@ window.addEventListener("load", function(){
     preloader.style.visibility = "hidden";
 
 });
+/* ==========================================
+   Preloader
+========================================== */
+
+window.addEventListener("load", function(){
+
+    const preloader = document.getElementById("preloader");
+
+    preloader.style.opacity = "0";
+    preloader.style.visibility = "hidden";
+
+});
